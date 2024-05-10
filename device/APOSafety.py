652053cc57564bca1fa16efbe1f675c6e1aed561
Jon Holtzman <holtz@nmsu.edu>
2024-05-09 20:10:31 -0600

    add alternate 2.5m status

--- device/APOSafety.py
+++ device/APOSafety.py
@@ -3,6 +3,7 @@ import pdb
 from datetime import datetime
 import time
 from logging import Logger
+import re
 
 messout = b"all"
 
@@ -15,7 +16,9 @@ class Safety :
         self.use35m = use35m
         self.use25m = use25m
 
-    def issafe(self) :
+    def stat(self,verbose=False) :
+        """ Get 3.5m and 2.5m status from 10.75.0.152
+        """
 
         sock = socket(AF_INET, SOCK_DGRAM)
         sock.sendto(messout, ('10.75.0.152', 6251))
@@ -35,6 +38,8 @@ class Safety :
         exec(stuff,globals(),ldict)
         encl35m=ldict['encl35m']
         encl25m=ldict['encl25m']
+        if verbose:
+            print('encl35m: {:s}  encl25m: {:s}'.format(encl35m,encl25m))
 
         try:
             encl35m
@@ -46,7 +51,6 @@ class Safety :
             stat35m="unknown"
         elif ( encl35m == "open" ) :
             stat35m="open"
-            safe35m = True
         else:
             stat35m="closed"
 
@@ -58,22 +62,52 @@ class Safety :
         safe25m = False
         if ( encl25m == "-1" ) :
             stat25m="unknown"
-            if self.use25m : safe=False
         elif ( encl25m == "16" ) :
             stat25m="open"
-            safe25m = True
         else:
             stat25m="closed"
+
+        return stat35m, stat25m
+
+    def encl25Open(self,verbose=False):
+        """ Get 2.5m status from 10.25.1.139
+        """
+        try :
+            s = socket()
+            s.connect(('10.25.1.139', 9990))
+            s.send(b'status\n')
+            time.sleep(1)
+            reply=s.recv(4096)
+            if (verbose) : print(reply)
+            stat25m = bool(int(re.search('encl25m=([0|1])', reply.decode()).group(1)))
+            if stat25m : return "open"
+            else : return "closed"
+        except :
+            return "unknown"
+
+    def issafe(self,verbose=False) :
+        """ Return whether is safe to be open based on 3.5m/2.5m as set up
+        """
+        stat35m, stat25m = self.stat(verbose=verbose)
+        stat25m = self.encl25Open(verbose=verbose)
+
+        safe25m = False
+        safe35m = False
+        if stat35m == "open" : safe35m = True
+        if stat25m == "open" : safe25m = True
 
         now=datetime.now()
         print("Enclosure: 3.5m",stat35m,", 2.5m",stat25m, "at",now.strftime("%d/%m/%Y %H:%M:%S"))
 
         if self.use35m and self.use25m :
-            return safe35m and safe25m
+            return safe35m or safe25m
         elif self.use35m :
             return safe35m
         elif self.use25m :
             return safe25m
         else :
             return False
+
+
+
  
